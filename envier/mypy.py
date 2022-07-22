f94b9cce302ef5628c45095baa15fa51ee377f08
Gabriele N. Tornetta <P403n1x87@users.noreply.github.com>
2022-07-22 15:50:38 +0100

    ci(mypy): fix typing (#14)

--- envier/mypy.py
+++ envier/mypy.py
@@ -1,16 +1,21 @@
-from typing import Callable
-from typing import Optional
-from typing import Type
+import typing as t
 
 from mypy.exprtotype import expr_to_unanalyzed_type
 from mypy.nodes import AssignmentStmt
 from mypy.nodes import CallExpr
 from mypy.nodes import ClassDef
+from mypy.nodes import MemberExpr
+from mypy.nodes import NameExpr
+from mypy.nodes import StrExpr
+from mypy.nodes import Var
 from mypy.plugin import ClassDefContext
 from mypy.plugin import MethodContext
 from mypy.plugin import Plugin
 from mypy.typeops import make_simplified_union
+from mypy.types import FunctionLike
 from mypy.types import Instance
+from mypy.types import ProperType
+from mypy.types import Type
 
 
 _envier_attr_makers = frozenset(
@@ -21,13 +26,14 @@ _envier_base_classes = frozenset({"envier.En", "envier.Env"})
 
 
 def _envier_attr_callback(ctx):
-    # type: (MethodContext) -> Type
+    # type: (MethodContext) -> ProperType
     arg_type = ctx.arg_types[0][0]
     if isinstance(arg_type, Instance):
         # WARNING: This returns an UnboundType which seems to match whatever!
         return expr_to_unanalyzed_type(ctx.args[0][0], ctx.api.options)
 
-    return make_simplified_union({_.ret_type for _ in arg_type.items})
+    assert isinstance(arg_type, FunctionLike), arg_type
+    return make_simplified_union({_.ret_type for _ in arg_type.items})  # type: ignore[arg-type]
 
 
 def _envier_base_class_callback(ctx):
@@ -38,13 +44,15 @@ def _envier_base_class_callback(ctx):
             if (
                 len(stmt.lvalues) != 1
                 or not isinstance(decl, CallExpr)
-                or decl.callee.expr.fullname not in _envier_base_classes
+                or t.cast(NameExpr, t.cast(MemberExpr, decl.callee).expr).fullname
+                not in _envier_base_classes
             ):
                 # We assume a single assignment per line, so this can't be an
                 # envier attribute maker.
                 continue
 
             (attr,) = stmt.lvalues
+            assert isinstance(attr, NameExpr) and isinstance(attr.node, Var), attr
 
             attr.node.type = ctx.api.anal_type(
                 expr_to_unanalyzed_type(decl.args[0], ctx.api.options)
@@ -65,13 +73,16 @@ def _envier_base_class_callback(ctx):
                 else:
                     return
 
+                # The value of the __item__ attribute must be a string.
+                assert isinstance(s.rvalue, StrExpr), s.rvalue
+
                 # Move the statement over from the class name to the item name
                 ctx.cls.info.names[s.rvalue.value] = ctx.cls.info.names.pop(stmt.name)
 
 
 class EnvierPlugin(Plugin):
     def get_method_hook(self, fullname):
-        # type: (str) -> Optional[Callable[[MethodContext], Type]]
+        # type: (str) -> t.Optional[t.Callable[[MethodContext], Type]]
         if fullname in _envier_attr_makers:
             # We use this callback to override the the method return value to
             # match the attribute value, which is also inferred by the `type`
@@ -81,7 +92,7 @@ class EnvierPlugin(Plugin):
         return None
 
     def get_base_class_hook(self, fullname):
-        # type: (str) -> Optional[Callable[[ClassDefContext], None]]
+        # type: (str) -> t.Optional[t.Callable[[ClassDefContext], None]]
         if fullname in _envier_base_classes:
             # We use this callback to override the class attribute types to
             # match the ones declared by the `type` argument of the Env methods.
@@ -91,5 +102,5 @@ class EnvierPlugin(Plugin):
 
 
 def plugin(version):
-    # type: (str) -> Plugin
+    # type: (str) -> t.Type[EnvierPlugin]
     return EnvierPlugin
